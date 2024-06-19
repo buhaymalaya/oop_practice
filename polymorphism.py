030b6f901c7e09198cfc227d42d3276fa05f3e50
Buhay Malaya <malayabuhay@gmail.com>
2024-06-19 10:10:52 -0400

    polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,68 @@
+# Polymorphism - Ability to take multiple forms
+# Child class inherits function from parent class can
+# override function and implement new set of rules to follow
+
+#  more than one parent or super class
+
+# super class = parent class
+# why parent? avoid redundancies
+# parent = inheritance
+# inherit by placing parent inside child parenthesis
+# also - def __init__ with same parameters as parent
+# underneath super().__init__(parameters above without self)
+
+class Dog:
+    def __init__(self, name, age, friendliness):
+        self.name = name
+        self.age = age
+        self.friendliness = friendliness
+    
+    def likes_walks(self):
+        return True
+    
+    def bark(self):
+        return 'Woof! default bark' # default bark
+    
+class Pug(Dog):
+    def __init__(self, name, age, friendliness):
+        super().__init__(name, age, friendliness)
+
+
+class Mutt(Dog):
+    def __init__(self, name, age, friendliness):
+        super().__init__(name, age, friendliness)
+
+    def shedding_amount(self):
+        return 0
+    
+    def bark(self): #polymorphism example
+        return "Oof oof! polymorphism"
+
+class Pit(Dog):
+    def __init__(self, name, age, friendliness):
+        super().__init__(name, age, friendliness)
+    
+    def fetch_ability(self):
+        if self.age < 2:
+            return 8
+        elif self.age < 10:
+            return 10
+        else:
+            return 4
+
+# MULTIPLE INHERITANCE/PARENTS/SUPER
+class PugPit(Pug, Pit):
+    def __init__(self, name, age, friendliness):
+        super().__init__(name, age, friendliness)
+
+    def bark(self): #polymorphism example
+        return "Arrff! Arrff! polymorphism"
+
+juicy = Pug('Juicy', 12, True)
+print(juicy.name, juicy.age, juicy.friendliness, juicy.likes_walks(), juicy.bark())
+
+catdog = PugPit('Felix', 10, 3)
+print(catdog.name, catdog.fetch_ability(), catdog.bark())
+
+courage = Mutt('Courage', 2, 8)
+print(courage.name, courage.bark(), courage.shedding_amount())
